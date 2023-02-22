eaa4e1c7c7cd993ff08f51e5d51f685cf860944e
defaultuser <defaultuser@pop-os.localdomain>
2023-02-22 12:17:24 -0800

    Added styling for NavHeader

--- src/NavHeader.js
+++ src/NavHeader.js
@@ -1,19 +1,21 @@
 import React from "react"
 import { Link } from 'react-router-dom'
 
+import './styles/NavHeader.css'
+
 function NavHeader() {
-    return (
-      <nav>
-        <ul>
-          <li>
-              <Link to="/">Game Grid</Link>
-          </li>
-          <li>
-              <Link to="/add-game">Add Game</Link>
-          </li>
-        </ul>
-      </nav>
-    )
+  return (
+    <nav>
+      <ul className="nav-header">
+        <li>
+            <Link className="nav-button" to="/">Game Grid</Link>
+        </li>
+        <li>
+            <Link className="nav-button" to="/add-game">Add Game</Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default NavHeader
