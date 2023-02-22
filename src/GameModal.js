cb39e9d851bb708f6431537dfcf4e4f749614312
defaultuser <defaultuser@pop-os.localdomain>
2023-02-22 11:39:36 -0800

    Improved GameModal and added close functionality for it

--- src/GameModal.js
+++ src/GameModal.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 
-function GameModal({ game }) {
+import './styles/GameModal.css'
+
+function GameModal({ game, onClose }) {
     return (
-        <div>
+        <div className="game-modal-overlay" onClick={onClose}>
+          <div className="game-modal-content">
             <h1>{game.title}</h1>
             <p>Description: {game.description}</p>
             <p>Play time (minutes): {game.playTime}</p>
             <p>Max number of players: {game.players}</p>
+            <button onClick={onClose}>Close</button>
+          </div>
         </div>
     )
 }
