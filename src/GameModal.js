d775149d07ab92d28cb25a232ea3950c9d59fcfb
Elliot Wolfe <Wolfy858@gmail.com>
2023-02-28 19:21:47 -0800

    Added a 'DeleteGame' button

--- src/GameModal.js
+++ src/GameModal.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
+import { ref, remove } from 'firebase/database';
+import database from './Firebase';
 
 import './styles/GameModal.css'
 
 function GameModal({ game, onClose }) {
-    return (
-        <div className="game-modal-overlay" onClick={onClose}>
-          <div className="game-modal-content">
-            <h1>{game.title}</h1>
-            <p>Description: {game.description}</p>
-            <p>Play time (minutes): {game.playTime}</p>
-            <p>Max number of players: {game.players}</p>
-            <button onClick={onClose}>Close</button>
-          </div>
+  const handleDeleteGame = () => {
+    const gameRef = ref(database, `/games/${game.id}`);
+    remove(gameRef);
+    onClose();
+  }
+
+  return (
+      <div className="game-modal-overlay" onClick={onClose}>
+        <div className="game-modal-content">
+          <h1>{game.title}</h1>
+          <p>Description: {game.description}</p>
+          <p>Play time (minutes): {game.playTime}</p>
+          <p>Max number of players: {game.players}</p>
+          <button className="delete-button" onClick={handleDeleteGame}>Delete Game</button>
         </div>
-    )
+      </div>
+  )
 }
 
 export default GameModal
