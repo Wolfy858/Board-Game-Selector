a3d548a1502647c9d235bc3e50c10e7da061e13d
defaultuser <defaultuser@pop-os.localdomain>
2023-02-28 18:28:41 -0800

    Improved GameGrid appearance and added a 'Clear all games' button to the bottom of it

--- src/GameGrid.js
+++ src/GameGrid.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import GameModal from './GameModal'
+import { ref, remove } from 'firebase/database';
+import database from './Firebase';
 
 import './styles/GameGrid.css'
 
 function GameGrid({ games }) {
   const [selectedGame, setSelectedGame] = useState(null);
+
+  const handleClearGames = () => {
+    const gamesRef = ref(database, '/games');
+    remove(gamesRef);
+  }
 
   const handleSelectGame = game => {
     setSelectedGame(game);
@@ -15,18 +22,22 @@ function GameGrid({ games }) {
   };
 
   return (
-    <div className="board-game-grid">
-      {games.map(game => (
-        <div
-          key={game.id}
-          className="game-thumbnail"
-          style={{ backgroundImage: `url("${game.thumbnail}")` }}
-          onClick={() => handleSelectGame(game)}
-        >
-          {/* <img src={game.imageUrl} alt={game.title} /> */}
-          <p>{game.title}</p>
-        </div>
-      ))}
+    <div>
+      <div className="board-game-grid">
+        {games.map(game => (
+          <div
+            key={game.id}
+            className="game-thumbnail"
+            style={{ backgroundImage: `url("${game.thumbnail}")` }}
+            onClick={() => handleSelectGame(game)}
+          >
+            {!game.thumbnail ? <p>{game.title}</p> : null}
+          </div>
+        ))}
+      </div>
+      <div className='grid-menu'>
+          <button className="clear-button" onClick={handleClearGames}>Clear All Games</button>
+      </div>
       {selectedGame && (
         <div className="modal-wrapper">
           <GameModal game={selectedGame} onClose={handleCloseModal} />
--- src/NavHeader.js
+++ src/NavHeader.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 import './styles/NavHeader.css'
 
-function NavHeader() {
+const NavHeader = () => {
   return (
     <nav>
       <ul className="nav-header">
