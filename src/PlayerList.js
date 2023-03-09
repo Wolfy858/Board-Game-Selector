4db6db3c3db97a892a38ad60c436fbf3ffa61acd
Elliot Wolfe <Wolfy858@gmail.com>
2023-03-08 16:25:41 -0800

    Started implementing a delete button for players. There's a bug where a player with more than one preference doesn't delete properly

--- src/PlayerList.js
+++ src/PlayerList.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { ref, onValue } from 'firebase/database';
+import { ref, onValue, remove, update } from 'firebase/database';
 import database from './Firebase';
 
 import './styles/PlayerList.css'
@@ -31,12 +31,36 @@ function PlayersList() {
     });
   }, []);
 
+  const handleDelete = (player) => {
+    console.log(player.id)
+    const playerRef = ref(database, `/players/${player.id}`);
+    remove(playerRef);
+
+    const gamesRef = ref(database, '/games');
+    onValue(gamesRef, (gamesSnapshot) => {
+      gamesSnapshot.forEach((gameSnapshot) => {
+        const game = gameSnapshot.val();
+        if (game.enjoyers && Object.values(game.enjoyers).includes(player.id)) {
+          const enjoyers = Object.entries(game.enjoyers).reduce((acc, [enjoyerId, enjoyerIdVal]) => {
+            if (enjoyerIdVal !== player.id) {
+              acc[enjoyerId] = enjoyerIdVal;
+            }
+            return acc;
+          }, {});
+          const gameRef = ref(database, `/games/${gameSnapshot.key}`);
+          update(gameRef, { enjoyers });
+        }
+      });
+    });
+  };
+
   return (
     <div className="players-list-container">
       <h2>Players</h2>
       <div className="players-list">
         {players.map((player) => (
           <div key={player.id} className="player-card">
+            <button className="delete-button" onClick={() => handleDelete(player)}>X</button>
             <div className="player-name">{player.name}</div>
             <div className="player-preferences">
               <span>Wants to play:</span>
