eed6e9fcd9c12bbbf004a059d2b702b1517dfdaf
Elliot Wolfe <Wolfy858@gmail.com>
2023-03-09 17:41:57 -0800

    Updated individual game deletion to also delete enjoyers' preferences

--- src/GameModal.js
+++ src/GameModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ref, remove, onValue, child, push } from 'firebase/database';
+import { ref, remove, onValue, child, push, update } from 'firebase/database';
 import database from './Firebase';
 
 import './styles/GameModal.css';
@@ -20,13 +20,36 @@ function GameModal({ game, onClose }) {
     return () => {
       playersListener();
     };
-  }, []); 
+  }, []);
 
   const handleDeleteGame = () => {
     const gameRef = ref(database, `/games/${game.id}`);
     remove(gameRef);
+
+    const playersRef = ref(database, '/players')
+    onValue(playersRef, (playersSnapshot) => {
+      playersSnapshot.forEach((playerSnapshot) => {
+        const player = playerSnapshot.val();
+        if (player.preferences && Object.values(player.preferences).includes(game.id)) {
+          const preferences = Object.entries(player.preferences).reduce((acc, [preferenceId, preferenceIdVal]) => {
+            if (preferenceIdVal !== game.id) {
+              acc[preferenceId] = preferenceIdVal;
+            }
+            return acc;
+          }, {});
+          const playerRef = ref(database, `/players/${playerSnapshot.key}`);
+          update(playerRef, { preferences });
+        }
+      });
+    })
     onClose();
   };
+
+  // const handleDeleteGame = () => {
+  //   const gameRef = ref(database, `/games/${game.id}`);
+  //   remove(gameRef);
+  //   onClose();
+  // };
 
   const handleAddPreference = () => {
     if (selectedPlayer) {
@@ -58,7 +81,7 @@ function GameModal({ game, onClose }) {
         </select>
         <button onClick={handleAddPreference}>Like</button>
 
-        <button className="delete-button" onClick={handleDeleteGame}>Delete Game</button>
+        <button className="delete-game-button" onClick={handleDeleteGame}>Delete Game</button>
       </div>
     </div>
   );
