3115eaa6dd3c3ab737114da18b9e5f6f5d43c658
Elliot Wolfe <Wolfy858@gmail.com>
2023-03-19 12:45:46 -0700

    Made the links for GameSearchForm and AddGameForm prettier

--- src/AddGame.js
+++ src/AddGame.js
@@ -1,19 +1,18 @@
 import { Routes, Outlet, Link } from 'react-router-dom';
+import './styles/AddGame.css'
 
 function AddGame() {
   return (
     <div>
-      <h2>Add Game</h2>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/add-game/search">Game Search Form</Link>
-          </li>
-          <li>
-            <Link to="/add-game/custom">Add Game Form</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="add-game-nav">
+        <Link to="/add-game/search" className="add-game-nav-link">
+          Game Search Form
+        </Link>
+        <Link to="/add-game/custom" className="add-game-nav-link">
+          Add Game Form
+        </Link>
+        
+      </div>
       <Outlet />
     </div>
   );
